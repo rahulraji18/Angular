ac22b722fa63d110547c9e3f1e8b541e439dcbdb
rahul.rajeevan-spericorn <rahul.rajeevan@spericorn.com>
2023-05-13 15:07:52 +0530

    string interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,10 @@ import { Component } from "@angular/core";
 })
 
 export class ServerComponent {
+    serverId: number = 18;
+    serverStatus: string = 'Offline';
 
+    getServerStatus() {
+        return this.serverStatus;
+    }
 }
